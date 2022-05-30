6c2a50983303a92444a4f3af9271cfc2dec1f8b5
williamwalleth <williamwall.eth@protonmail.com>
2022-05-30 20:57:47 +0100

    Init

--- packages/eslint-config-acme/index.js
+++ packages/eslint-config-acme/index.js
@@ -1,7 +1,6 @@
 module.exports = {
-  extends: ["next", "prettier"],
+  extends: ["prettier"],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+    "@next/next/no-html-link-for-pages": "off"
   },
 };
